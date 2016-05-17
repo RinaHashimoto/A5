e1739436187f4869196c3632ad095e2bfb6400a3
g14909hr <g14909hr@m20203.tsuda.ac.jp>
2016-05-17 16:30:15 +0900

    RinaHashimotoが999まで実装しました。テストも完了しています。

--- A5/test/inttoeng/IntToEngTest.java
+++ A5/test/inttoeng/IntToEngTest.java
@@ -46,7 +46,43 @@ public class IntToEngTest {
 	public void translate20to90() {
 		IntToEng ite = new IntToEng();
 		int[] numbers = {20,30,40,50,60,70,80,90};
-		String[] expected = {"twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
+		String[] expected = {"twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
+		for (int i = 0; i < expected.length; i++) {
+			String actual = ite.translateEng(numbers[i]);
+			assertThat(actual, is(expected[i]));
+		}
+	}
+	
+	@Test
+	public void translate21to30() {
+		IntToEng ite = new IntToEng();
+		int[] numbers = new int[10];
+		for (int i = 0; i < numbers.length; i++) numbers[i] = i+21;
+		String[] expected = {"twenty one","twenty two","twenty three","twenty four","twenty five","twenty six","twenty seven","twenty eight", "twenty nine", "thirty"};
+		for (int i = 0; i < expected.length; i++) {
+			String actual = ite.translateEng(numbers[i]);
+			assertThat(actual, is(expected[i]));
+		}
+	}
+	
+	@Test
+	public void translate91to99() {
+		IntToEng ite = new IntToEng();
+		int[] numbers = new int[9];
+		for (int i = 0; i < numbers.length; i++) numbers[i] = i+91;
+		String[] expected = {"ninety one","ninety two","ninety three","ninety four","ninety five","ninety six","ninety seven","ninety eight", "ninety nine"};
+		for (int i = 0; i < expected.length; i++) {
+			String actual = ite.translateEng(numbers[i]);
+			assertThat(actual, is(expected[i]));
+		}
+	}
+	
+	@Test
+	public void translate100to109() {
+		IntToEng ite = new IntToEng();
+		int[] numbers = new int[10];
+		for (int i = 0; i < numbers.length; i++) numbers[i] = i+100;
+		String[] expected = {"one hundred","one hundred one","one hundred two","one hundred three","one hundred four","one hundred five","one hundred six","one hundred seven","one hundred eight", "one hundred nine"};
 		for (int i = 0; i < expected.length; i++) {
 			String actual = ite.translateEng(numbers[i]);
 			assertThat(actual, is(expected[i]));
